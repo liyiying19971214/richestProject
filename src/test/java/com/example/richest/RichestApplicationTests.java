4fb4dbe8aa5ff5de19b7e2af9c1c287dc56734f9
lyy <1519583238@qq.com>
2022-01-17 16:01:18 +0800

    first commit

---
+++ src/test/java/com/example/richest/RichestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.richest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RichestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
